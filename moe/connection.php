917d492ea20777c619618c4aca4fb56a08696be1
Kyarakuta07 <alifshatargustian@gmail.com>
2025-12-04 15:15:41 +0700

    connection databse

--- moe/connection.php
+++ moe/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "moe";
+$username = "akevyrfm_Matthew";
+$password = "#Mandala1968";
+$dbname = "akevyrfm_moe_live";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
